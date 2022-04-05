1f7da4567b7124c001a6026204b52e43abbb92fb
Dima Dziza <dzizadima27@gmail.com>
2022-04-05 17:42:41 +0300

    add more weather button

--- scrypts.js
+++ scrypts.js
@@ -1,4 +1,4 @@
-
+const monthArr = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 function getWeather(location){
    let latitude = location.latitude;
    let longitude = location.longitude;
@@ -8,15 +8,97 @@ function getWeather(location){
    lat_box.innerHTML = location.latitude
    lon_box.innerHTML = location.longitude
 
-   return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=c708b1623179bb92fb6654448ed0ba58&units=metric`).then(response => response.json())
+   loadingToggle()
+   return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=c708b1623179bb92fb6654448ed0ba58&units=metric`)
+   .then(response => response.json())
+}
+
+function getWeatherDates(location){
+   let latitude = location.latitude;
+   let longitude = location.longitude;
+
+   loadingToggle()
+   return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&appid=c708b1623179bb92fb6654448ed0ba58&units=metric`)
+   .then(response => response.json())
+}
+
+function renderTodayWeather(data){
+   document.getElementById("city").innerHTML = data.name
+   
+   loadingToggle();
+
+   console.log(data)
+
+   var date = new Date(data.dt * 1000),
+   month = date.getMonth(),
+   monthFull = monthArr[month];
+   
+   console.log("rrr",   date.toDateString())
+   console.log("rrr",   date.getDate())
+   console.log("rrr", monthFull)
+   let html = ` 
+      <div class="card weather-today">
+         <div class="date">
+            ${date.getDate()} ${monthFull} (Today)
+         </div>
+         <div class="icon">
+            <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+         </div>
+         <h4 class="temp">Temperature ${Math.round(data.main.temp)}</h4>
+         <span class="feels">Feels like ${Math.round(data.main.feels_like)}</span>
+         <span class="wind">Wind speed ${data.wind.speed}</span>
+      </div>
+      <div class="card card-more" onclick="displayMoreWeather()">
+         <img src="img/plus-circle.svg" />
+      </div>
+      `;
+   document.getElementById("weather").innerHTML = html
+}
+
+function displayMoreWeather(){
+   getLocationPromise.then(location => getWeatherDates(location)).then((response)=>{
+      console.log("days",response)
+      console.log("days",response.daily[0])
+
+      for(let i=1,j=response.daily.length; i<j; i++){
+         console.log(response.daily[i])
+         let day = response.daily[i];
+         var date = new Date(response.daily[i].dt * 1000),
+         month = date.getMonth(),
+         monthFull = monthArr[month];
+         
+         console.log("toDateString",   date.toDateString())
+         console.log("getDate",   date.getDate())
+         console.log("monthFull", monthFull)
+
+         let html = ` 
+         <div class="card">
+            <div class="date">
+               ${date.getDate()} ${monthFull}
+            </div>
+            <div class="icon">
+               <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="">
+            </div>
+            <h4 class="temp">Temperature ${Math.round(day.temp.day)}</h4>
+            <span class="feels">Feels like ${Math.round(day.feels_like.day)}</span>
+            <span class="wind">Wind speed ${day.wind_speed}</span>
+         </div>`;
+         document.getElementsByClassName("card-more")[0].style.display="none";
+         document.getElementById("weather").innerHTML += html
+      }
+
+      loadingToggle()
+   })
+}
+
+function loadingToggle(){
+   let spinner = document.getElementById("spinner");
+   spinner.hasAttribute("hidden") ? spinner.removeAttribute('hidden') : spinner.setAttribute('hidden', '');
 }
 
 let getLocationPromise = new Promise((resolve, reject) => {
    if(navigator.geolocation) {
        navigator.geolocation.getCurrentPosition(function (position) {
-
-           let spinner = document.getElementById("spinner");
-           spinner.removeAttribute('hidden');  
 
            lat = position.coords.latitude
            long = position.coords.longitude
@@ -29,24 +111,8 @@ let getLocationPromise = new Promise((resolve, reject) => {
    }
 })
 
-
-getLocationPromise.then(location => getWeather(location)).then((response)=>{
-   document.getElementById("city").innerHTML = response.name
-   spinner.setAttribute('hidden', '');
-   console.log(response)
-   console.log("GGg",response.main.temp)
-   console.log("icon", `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`)
-   let html = ` 
-<div class="card">
-   <div class="icon">
-      <img src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" alt="">
-   </div>
-   <h4 class="temp">Temperature ${Math.round(response.main.temp)}</h4>
-   <span class="feels">Feels like ${Math.round(response.main.feels_like)}</span>
-   <span class="wind">Wind speed ${response.wind.speed}</span>
-</div>`;
-document.getElementById("weather").innerHTML = html
-}).catch((err) => {
+getLocationPromise.then(location => getWeather(location)).then(response => renderTodayWeather(response)).then(location => getWeatherDates(location))
+.catch((err) => {
    console.log(err)
 })
 
